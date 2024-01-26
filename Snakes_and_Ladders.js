2483f60eb7f36617777295243ab53e36ee4f3b0a
31812 <31812@ma-web.nl>
2024-01-26 15:22:31 +0100

    4/5

--- Snakes_and_Ladders.js
+++ Snakes_and_Ladders.js
@@ -15,8 +15,48 @@ let g = canvas.getContext("2d")
 
 let uiWindow = createRect(600, 200, 300, 300);
 
-let gameState = gamestate_start
-let ingamegameState = ingamestate_start
+let gameState = gamestate_start;
+let ingamegameState = ingamestate_start;
+
+let images = {};
+
+function imagesLoaded(){
+    initgame()
+    draw()
+}
+
+function loadImages()
+{
+    let sources = [
+        "img/dice1.png", "img/dice2.png", "img/dice3.png", "img/dice4.png", "img/dice5.png", "img/dice6.png",
+        "img/pawn0.png", "img/pawn1.png", "img/pawn2.png", "img/pawn3.png", 
+        "img/snakes.png", 
+        "img/trophy.png", 
+        "img/window.png", 
+    ];
+    
+    let scope = this;
+
+    let loaded = 0;
+    for (let i = 0; i < sources.length; i++)
+    {
+        let img = new Image();
+
+
+        img.onload = function ()
+        {
+            loaded++;
+            if (loaded == sources.length)
+            {
+                imagesLoaded();
+            }
+        };
+        img.src = sources[i];
+
+        images[ sources[i].replace("img/","")] = img;
+    }
+}
+
 
 function createRect(x,y,w,h)
 {
@@ -78,52 +118,48 @@ function createBoardPositions()
     }
 }
 
-function drawGameStart(){
-    for (let index = 0; index < array.length; index++) {
-        const element = array[index];
-        
-    }
-}
-
 function initgame(){
-    
+    createBoardPositions()
+
     for (let index = 0; index < 4; index++) {
         let button = createRect(uiWindow.x+5 +(index*50),uiWindow.y+50,50,50);
         playerAmountButtons.push(button)
-        button.playerAmount=i+1;
-    }
-    createBoardPositions()
-}
-
-
-function drawGameStart(){
-    for(let i=0; i<playerAmountButtons.length; i++)
-    {
-        let pos = playerAmountButtons[i];
-        g.fillstyle = "#004400";
-        g.fillRect(pos.x, pos.y, pos.w, pos.h);
-        g.fillstyle = "#FFFFFF";
-        g.fillText((i+1)+"",pos.x, pos.y+20);
+        button.playerAmount=index+1;
     }
 }
 
-function drawIngame()
-{
-    for(let i=0; i<boardPositions.length; i++)
-    {
+
+function drawGameStart() {
+    for (let i = 0; i < playerAmountButtons.length; i++) {
+        let button = playerAmountButtons[i];
+        g.fillStyle = "#004400";
+        g.fillRect(button.x, button.y, button.w, button.h);
+        g.fillStyle = "#FFFFFF";
+        g.fillText(button.playerAmount + "", button.x, button.y + 20);
+        g.drawImage(images["pawn"+i+".png"],button.x,button.y,button.w,button.h)
+    }
+}
+
+function drawIngame() {
+    for (let i = 0; i < boardPositions.length; i++) {
         let pos = boardPositions[i];
-        g.fillstyle = "#004400";
+        g.fillStyle = "#004400";
         g.fillRect(pos.x, pos.y, pos.w, pos.h);
-        g.fillstyle = "#FFFFFF";
-        g.fillText((i+1)+"",pos.x, pos.y+20);
+        g.fillStyle = "#FFFFFF";
+        g.fillText((i + 1) + "", pos.x, pos.y + 20);
     }
 }
 
-function drawGameOver(){
-    clearCanvas()
-    drawGameStart()
+function draw(){
+    clearCanvas();
+    
+    if(gameState = gamestate_ingame){
+    drawGameStart();
+    }
+    
+    if(gameState = gamestate_start){
     drawIngame()
+    }
 }
 
-initgame()
-draw()
+loadImages()
